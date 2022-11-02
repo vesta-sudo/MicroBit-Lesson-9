b63aab67d5c72a9383acb1a85865b3f82febbf73
vesta-sudo <115300048+vesta-sudo@users.noreply.github.com>
2022-11-02 14:08:22 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,23 @@
-
+input.onPinPressed(TouchPin.P0, function () {
+    Binary = "" + Binary + "1"
+    basic.showString(Binary)
+    Decimal = Decimal * 2 + 1
+})
+input.onPinPressed(TouchPin.P2, function () {
+	
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Decimal)
+})
+input.onButtonPressed(Button.B, function () {
+    Binary = "" + Binary + "0"
+    basic.showString(Binary)
+    Decimal = Decimal * 2
+})
+input.onPinPressed(TouchPin.P1, function () {
+	
+})
+let Binary = ""
+let Decimal = 0
+Decimal = 0
+Binary = ""
