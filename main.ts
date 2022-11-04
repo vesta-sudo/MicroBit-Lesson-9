2d6f9e2ba51c7505a682cd2247ec90259a602ffe
vesta-sudo <115300048+vesta-sudo@users.noreply.github.com>
2022-11-03 21:29:15 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,3 +22,18 @@ let Binary = ""
 let Decimal = 0
 Decimal = 0
 Binary = ""
+basic.forever(function () {
+    if (!(input.pinIsPressed(TouchPin.P0))) {
+        Decimal = Decimal - 4
+    }
+})
+basic.forever(function () {
+    if (!(input.pinIsPressed(TouchPin.P1))) {
+        Decimal = Decimal - 2
+    }
+})
+basic.forever(function () {
+    if (!(input.pinIsPressed(TouchPin.P2))) {
+        Decimal = Decimal - 1
+    }
+})
